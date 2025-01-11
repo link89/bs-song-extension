8e5c337138f3c9744037201e003f4b00a536dd42
weihong.xu <xuweihong.cn@gmail.com>
2025-01-11 14:54:27 +0800

    content script method to scan map ids

--- src/contentScript.ts
+++ src/contentScript.ts
@@ -1,19 +1,9 @@
-(function() {
-  // Get all <a> tags in the page
-  const anchorTags = document.querySelectorAll("a[href^='beatsaver://']");
-
-  // Extract the IDs from the links
-  const beatSaverIds: string[] = [];
-  anchorTags.forEach(a => {
-    const url = a.getAttribute("href");
-    // Format is beatsaver://{id}
-    if (!url) return;
-    const id = url.replace("beatsaver://", "");
-    beatSaverIds.push(id);
-  });
-
-  // Send the IDs to the background/panel script
-  if (beatSaverIds.length > 0) {
-    chrome.runtime.sendMessage({ type: "foundBeatSaverIds", ids: beatSaverIds });
+chrome.runtime.onMessage.addListener((msg, sender, sendResponse) => {
+  if (msg.type === "SCAN_BS_MAPS") {
+    const bsMapIds = Array.from(document.querySelectorAll("a"))
+      .map(a => a.href)
+      .filter(href => href.startsWith("beatsaver://"))
+      .map(href => new URL(href).pathname.split("/").pop());
+    sendResponse({ bsMapIds });
   }
-})();
+});
